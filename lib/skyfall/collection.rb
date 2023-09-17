3d500f03b8679ac57181b0d0c1d65bb84f808c7b
Kuba Suder <jakub.suder@gmail.com>
2023-09-17 14:04:52 +0200

    added threadgate record type, sorted collection lists

--- lib/skyfall/collection.rb
+++ lib/skyfall/collection.rb
@@ -1,14 +1,15 @@
 module Skyfall
   module Collection
-    BSKY_POST = "app.bsky.feed.post"
-    BSKY_LIKE = "app.bsky.feed.like"
-    BSKY_FOLLOW = "app.bsky.graph.follow"
-    BSKY_REPOST = "app.bsky.feed.repost"
-    BSKY_BLOCK = "app.bsky.graph.block"
-    BSKY_PROFILE = "app.bsky.actor.profile"
-    BSKY_LIST = "app.bsky.graph.list"
-    BSKY_LISTITEM = "app.bsky.graph.listitem"
-    BSKY_LISTBLOCK = "app.bsky.graph.listblock"
-    BSKY_FEED = "app.bsky.feed.generator"
+    BSKY_PROFILE    = "app.bsky.actor.profile"
+    BSKY_FEED       = "app.bsky.feed.generator"
+    BSKY_LIKE       = "app.bsky.feed.like"
+    BSKY_POST       = "app.bsky.feed.post"
+    BSKY_REPOST     = "app.bsky.feed.repost"
+    BSKY_THREADGATE = "app.bsky.feed.threadgate"
+    BSKY_BLOCK      = "app.bsky.graph.block"
+    BSKY_FOLLOW     = "app.bsky.graph.follow"
+    BSKY_LIST       = "app.bsky.graph.list"
+    BSKY_LISTBLOCK  = "app.bsky.graph.listblock"
+    BSKY_LISTITEM   = "app.bsky.graph.listitem"
   end
 end
--- lib/skyfall/operation.rb
+++ lib/skyfall/operation.rb
@@ -41,16 +41,17 @@ module Skyfall
 
     def type
       case collection
-      when Collection::BSKY_POST then :bsky_post
-      when Collection::BSKY_LIKE then :bsky_like
-      when Collection::BSKY_FOLLOW then :bsky_follow
-      when Collection::BSKY_REPOST then :bsky_repost
-      when Collection::BSKY_BLOCK then :bsky_block
-      when Collection::BSKY_PROFILE then :bsky_profile
-      when Collection::BSKY_LIST then :bsky_list
-      when Collection::BSKY_LISTITEM then :bsky_listitem
-      when Collection::BSKY_LISTBLOCK then :bsky_listblock
-      when Collection::BSKY_FEED then :bsky_feed
+      when Collection::BSKY_BLOCK      then :bsky_block
+      when Collection::BSKY_FEED       then :bsky_feed
+      when Collection::BSKY_FOLLOW     then :bsky_follow
+      when Collection::BSKY_LIKE       then :bsky_like
+      when Collection::BSKY_LIST       then :bsky_list
+      when Collection::BSKY_LISTBLOCK  then :bsky_listblock
+      when Collection::BSKY_LISTITEM   then :bsky_listitem
+      when Collection::BSKY_POST       then :bsky_post
+      when Collection::BSKY_PROFILE    then :bsky_profile
+      when Collection::BSKY_REPOST     then :bsky_repost
+      when Collection::BSKY_THREADGATE then :bsky_threadgate
       else :unknown
       end
     end
