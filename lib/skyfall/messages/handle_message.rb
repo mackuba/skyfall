dc9d446dcb10560b5b0bbf03d765d2beaff859f6
Kuba Suder <jakub.suder@gmail.com>
2024-06-06 23:39:41 +0200

    added note about deprecation of #tombstone and #handle

--- lib/skyfall/messages/handle_message.rb
+++ lib/skyfall/messages/handle_message.rb
@@ -1,4 +1,9 @@
 module Skyfall
+
+  #
+  # Note: this event type is deprecated and will stop being emitted at some point.
+  # You should instead listen for 'identity' events (Skyfall::IdentityMessage).
+  #
   class HandleMessage < WebsocketMessage
     def handle
       @data_object['handle']
--- lib/skyfall/messages/tombstone_message.rb
+++ lib/skyfall/messages/tombstone_message.rb
@@ -1,4 +1,9 @@
 module Skyfall
+
+  #
+  # Note: this event type is deprecated and will stop being emitted at some point.
+  # You should instead listen for 'account' events (Skyfall::AccountMessage).
+  #
   class TombstoneMessage < WebsocketMessage
   end
 end
