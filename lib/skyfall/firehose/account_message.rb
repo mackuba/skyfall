8b00c7749902db636251e97238a6a0431882000b
Kuba Suder <jakub.suder@gmail.com>
2026-01-11 02:29:53 +0200

    moved active? and status in AccountMessage to ivars
    so that they are included in inspect output

--- lib/skyfall/firehose/account_message.rb
+++ lib/skyfall/firehose/account_message.rb
@@ -2,12 +2,18 @@ require_relative '../firehose'
 
 module Skyfall
   class Firehose::AccountMessage < Firehose::Message
-    def active?
-      @data_object['active']
+    def initialize(type_object, data_object)
+      super
+      raise DecodeError.new("Missing event details") if @data_object['active'].nil?
+
+      @active = @data_object['active']
+      @status = @data_object['status']&.to_sym
     end
 
-    def status
-      @data_object['status']&.to_sym
+    def active?
+      @active
     end
+
+    attr_reader :status
   end
 end
